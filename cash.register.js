73bc38ce1d1339f338403405f6beb18ef2297a77
James Gore <83005220+thejamesgore@users.noreply.github.com>
2022-01-29 03:26:53 +0000

    base cash register solution

--- cash.register.js
+++ cash.register.js
@@ -1,18 +1,17 @@
-let currencyConverter = [
-  ['PENNY', 0.01],
-  ['NICKEL', 0.05],
-  ['DIME', 0.1],
-  ['QUARTER', 0.25],
-  ['ONE', 1],
-  ['FIVE', 5],
-  ['TEN', 10],
-  ['TWENTY', 20],
-  ['ONE HUNDRED', 100],
+const money = [
+  ['Penny', 0.01],
+  ['Nickle', 0.05],
+  ['Dime', 0.1],
+  ['Quarter', 0.25],
+  ['One', 1],
+  ['Five', 5],
+  ['Ten', 10],
+  ['Twenty', 20],
+  ['One Hundred', 100],
 ]
 
 function checkCashRegister(price, cash, cid) {
   let change = price - cash
-  let status = ''
 
   return change
 }
